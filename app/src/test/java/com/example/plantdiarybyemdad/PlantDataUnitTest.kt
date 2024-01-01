abbcd0cfc17d8a862bfb13b98bb3091912f985cf
emdad <emdad@ergo-ventures.com>
2024-01-01 23:49:46 +0600

    PlantDataUnit test creating BDD given when then

--- app/src/test/java/com/example/plantdiarybyemdad/PlantDataUnitTest.kt
+++ app/src/test/java/com/example/plantdiarybyemdad/PlantDataUnitTest.kt
@@ -19,13 +19,62 @@ class PlantDataUnitTest {
 
 
     @get:Rule
-    var rule: TestRule = InstantTaskExecutorRule()
-    lateinit var mvm: MainViewModel
+    var rule:TestRule= InstantTaskExecutorRule() // this will make testing live data observables a little bit easier
+    lateinit var mvm:MainViewModel
+
 
     @Test
     fun confirmEasternRedbud_outputsEasternRedbud() {
         var plant: Plant = Plant("Cercis", "canadesis", "Eastern Redbud")
         assertEquals("Eastern Redbud", plant.toString())
     }
+
+    @Test
+    fun searchForRedbud_returnsRedbud(){
+        givenAFeedOfPlantDataAreAvailable()
+        whenSearchForRedbud()
+        thenResultContainsEasternRedbud()
+    }
+
+    private fun givenAFeedOfPlantDataAreAvailable() {
+        mvm = MainViewModel()
+    }
+
+    private fun whenSearchForRedbud() {
+        mvm.fetchPlants("Redbud")
+    }
+
+    private fun thenResultContainsEasternRedbud() {
+        var redbudFound=false
+        mvm.plants.observeForever{
+            assertNotNull(it) // did we get something back?
+            assertTrue(it.size()>0) // did we get a collection back? and is that collection not empty?
+            it.forEach{
+                if(it.genus=="Cercis" && it.species=="canadencis" && it.common.contains("Eastern Redbud")){
+                    redbudFound=true
+                }
+            }
+        }
+        assertTrue(redbudFound)
+    }
+
+
+    @Test
+    fun searchForGarbage_returnsNothing(){
+        givenAFeedOfPlantDataAreAvailable()
+        whenISearchForGarbage()
+        thenIGetZeroResult()
+    }
+
+    private fun whenISearchForGarbage() {
+        mvm.fetchPlants("kjalsjdflajdf")
+    }
+
+    private fun thenIGetZeroResult() {
+        mvm.plants.observeForever{
+            assertEquals(0,it.size)
+        }
+    }
+
 
 }
