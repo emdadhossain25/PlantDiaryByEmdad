45af551ce58ee2d883e49cb97bfde051bc7e0f1a
emdad <emdad@ergo-ventures.com>
2024-01-10 00:35:21 +0600

    get thumbnail from camera in android/kotlin

--- app/src/main/java/com/example/plantdiarybyemdad/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/plantdiarybyemdad/ui/main/MainFragment.kt
@@ -1,17 +1,30 @@
 package com.example.plantdiarybyemdad.ui.main
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
+import com.example.plantdiarybyemdad.Manifest
 import com.example.plantdiarybyemdad.R
 
 class MainFragment : Fragment() {
+
+    private val CAMERA_REQUEST_CODE: Int = 1998
+    val CAMERA_PERMISSION_REQUEST_CODE = 1997
 
     companion object {
         fun newInstance() = MainFragment()
@@ -19,6 +32,8 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
     var actPlantName: AutoCompleteTextView? = null
+    var btnTakePhoto: ImageButton? = null
+    var imgPlant: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +46,8 @@ class MainFragment : Fragment() {
     ): View {
         var root = inflater.inflate(R.layout.fragment_main, container, false)
         actPlantName = root.findViewById(R.id.actPlantName)
+        btnTakePhoto = root.findViewById(R.id.btnTakePhoto)
+        imgPlant = root.findViewById(R.id.imgPlant)
         return root
 
     }
@@ -46,6 +63,71 @@ class MainFragment : Fragment() {
                 )
             )
         })
+        btnTakePhoto?.setOnClickListener {
+            prepTakePhoto()
+        }
+
+
+    }
+
+    /**
+     * See if we have permission or not
+     */
+    private fun prepTakePhoto() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            takePhoto()
+        } else {
+            val permissionRequest = arrayOf(android.Manifest.permission.CAMERA)
+            requestPermissions(permissionRequest, CAMERA_PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted, let's do stuff.
+                    takePhoto()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Unable to take photo without permission",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+            }
+        }
+
+    }
+
+    private fun takePhoto() {
+
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireContext().packageManager)?.also {
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                // now we can get the thumbnail
+                val imageBitmap = data!!.extras!!.get("data") as Bitmap
+                imgPlant?.setImageBitmap(imageBitmap)
+            }
+        }
     }
 
 }
